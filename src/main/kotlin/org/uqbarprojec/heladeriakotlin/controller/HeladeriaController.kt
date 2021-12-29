48b99880b025c33e9706e7bc2ab0f40d570878ed
alete89 <alejandrodini@mural.co>
2021-12-29 14:01:29 -0300

    agrego nuevas rutas y CORS

--- src/main/kotlin/org/uqbarprojec/heladeriakotlin/controller/HeladeriaController.kt
+++ src/main/kotlin/org/uqbarprojec/heladeriakotlin/controller/HeladeriaController.kt
@@ -1,18 +1,47 @@
 package org.uqbarprojec.heladeriakotlin.controller
 
-import org.uqbarprojec.heladeriakotlin.model.Heladeria
-import org.uqbarprojec.heladeriakotlin.service.HeladeriaService
 import org.springframework.web.bind.annotation.*
+import org.uqbarprojec.heladeriakotlin.model.Duenio
+import org.uqbarprojec.heladeriakotlin.model.Heladeria
+import org.uqbarprojec.heladeriakotlin.model.HeladeriaDTO
+import org.uqbarprojec.heladeriakotlin.service.DuenioService
+import org.uqbarprojec.heladeriakotlin.service.HeladeriaService
 
+@CrossOrigin
 @RestController
-class HeladeriaController(private val heladeriaService: HeladeriaService) {
-    @GetMapping("/heladeria/{text}")
-    fun getHeladeria(@PathVariable text: String): List<Heladeria> {
-        return heladeriaService.findByNombre(text)
+class HeladeriaController(private val heladeriaService: HeladeriaService, private val duenioService: DuenioService) {
+    @GetMapping("/heladerias/buscar/{text}")
+    fun getHeladeria(@PathVariable text: String): List<HeladeriaDTO> {
+        return heladeriaService.findByNombre(text).map { HeladeriaDTO.fromHeladeria(it) }
     }
 
-    @PostMapping("/crear")
-    fun createHeladeria(@RequestBody heladeria: Heladeria): Heladeria {
-        return heladeriaService.validarYGuardar(heladeria)
+    @GetMapping("/heladerias/id/{id}")
+    fun getHeladeria(@PathVariable id: Long): Heladeria {
+        return heladeriaService.findById(id)
+    }
+
+    @GetMapping("/duenios")
+    fun getDuenios(): List<Duenio> {
+        return duenioService.findAll()
+    }
+
+    @PostMapping("/duenios")
+    fun crearDuenio(@RequestBody duenio: Duenio): Duenio {
+        return duenioService.validarYGuardar(duenio)
+    }
+
+    @PatchMapping("/heladerias/{heladeriaId}")
+    fun actualizarHeladeria(@RequestBody heladeria: Heladeria, @PathVariable heladeriaId: Long): Heladeria {
+        return heladeriaService.actualizar(heladeriaId, heladeria)
+    }
+
+    @PostMapping("/heladerias/{heladeriaId}/gustos")
+    fun agregarGustos(@RequestBody gustos: MutableMap<String, Int>, @PathVariable heladeriaId: Long): Heladeria {
+        return heladeriaService.agregarGustos(heladeriaId, gustos)
+    }
+
+    @DeleteMapping("/heladerias/{heladeriaId}/gustos")
+    fun eliminarGustos(@RequestBody gustos: MutableMap<String, Int>, @PathVariable heladeriaId: Long): Heladeria {
+        return heladeriaService.eliminarGustos(heladeriaId, gustos)
     }
 }
