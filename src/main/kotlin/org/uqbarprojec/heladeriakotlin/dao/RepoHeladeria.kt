5969a58007a2e1917afce907518b9d65d9de0902
alete89 <alejandrodini@mural.co>
2021-12-29 14:06:59 -0300

    arregla el repo trayendo los field necesarios

--- src/main/kotlin/org/uqbarprojec/heladeriakotlin/dao/RepoHeladeria.kt
+++ src/main/kotlin/org/uqbarprojec/heladeriakotlin/dao/RepoHeladeria.kt
@@ -1,11 +1,17 @@
 package org.uqbarprojec.heladeriakotlin.dao
 
-import org.uqbarprojec.heladeriakotlin.model.Heladeria
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import org.uqbarprojec.heladeriakotlin.model.Heladeria
+import java.util.*
 
 @Repository
 interface RepoHeladeria : CrudRepository<Heladeria, Long> {
 
+    @EntityGraph(attributePaths = ["duenio"])
     fun findByNombreContaining(text: String): List<Heladeria>
+
+    @EntityGraph(attributePaths = ["duenio", "gustos"])
+    override fun findById(id: Long): Optional<Heladeria>
 }
