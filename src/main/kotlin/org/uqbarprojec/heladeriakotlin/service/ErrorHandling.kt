c9092950c9a3e83c46290b1791afdff49b8f6dd6
alete89 <alejandrodini@mural.co>
2021-12-29 13:57:41 -0300

    limpio clases ErrorHandling

--- src/main/kotlin/org/uqbarprojec/heladeriakotlin/service/ErrorHandling.kt
+++ src/main/kotlin/org/uqbarprojec/heladeriakotlin/service/ErrorHandling.kt
@@ -2,14 +2,9 @@ package org.uqbarprojec.heladeriakotlin.service
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
-import java.lang.RuntimeException
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-class UserException(message: String) : RuntimeException(message) {
-}
+class UserException(message: String) : RuntimeException(message)
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-class NotFoundException(message: String) : RuntimeException(message) {
-}
-
-
+class NotFoundException(message: String) : RuntimeException(message)
