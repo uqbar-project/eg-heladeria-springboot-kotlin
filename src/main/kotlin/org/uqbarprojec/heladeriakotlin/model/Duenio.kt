987e05948d9d15c93cd242f9be9fd0ba93cf46ff
nicovio <nicolasviotti90@gmail.com>
2022-03-31 03:00:26 -0300

    Cambia estrategia de generación de ids

--- src/main/kotlin/org/uqbarprojec/heladeriakotlin/model/Duenio.kt
+++ src/main/kotlin/org/uqbarprojec/heladeriakotlin/model/Duenio.kt
@@ -1,5 +1,7 @@
 package org.uqbarprojec.heladeriakotlin.model
 
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.Parameter
 import org.uqbarprojec.heladeriakotlin.service.UserException
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -8,7 +10,14 @@ import javax.persistence.Id
 @Entity
 class Duenio(val nombreCompleto: String) {
     @Id
-    @GeneratedValue
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+        name = "sequence-generator",
+        strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+        parameters = [Parameter(name = "sequence_name", value = "duenio_sequence"),
+            Parameter(name = "initial_value", value = "1"),
+            Parameter(name = "increment_size", value = "1")]
+    )
     val id: Long = 0
 
     fun validar() {
