ff22f8bbc14c2e7c9fa8783ba9ff20089c9dd17d
nicovio <nicolasviotti90@gmail.com>
2022-04-01 02:52:52 -0300

    Agrego algunos tests

--- src/test/kotlin/org/uqbarprojec/heladeriakotlin/HeladeriaApplicationTests.kt
+++ src/test/kotlin/org/uqbarprojec/heladeriakotlin/HeladeriaApplicationTests.kt
@@ -1,5 +1,6 @@
 package org.uqbarprojec.heladeriakotlin
 
+import org.json.JSONObject
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -8,8 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import javax.transaction.Transactional
 
 
 @SpringBootTest
@@ -36,4 +38,63 @@ class HeladeriaApplicationTests(@Autowired val mockMvc: MockMvc) {
             .andExpect(jsonPath("$.length()").value(3))
     }
 
+    @Test
+    fun `Buscar una heladeria por nombre no encuentra ninguna que coincida`() {
+        mockMvc
+            .perform(get("/heladerias/buscar/{nombre}", "inexistente"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.length()").value(0))
+    }
+
+    @Test
+    fun `Buscar una heladeria por id inexistente devuelve not found`() {
+        mockMvc
+            .perform(get("/heladerias/id/{id}", "999"))
+            .andExpect(status().isNotFound)
+    }
+
+    @Test
+    fun `Buscar heladeria pasando un id invalido devuelve bad request`() {
+        mockMvc
+            .perform(get("/heladerias/id/{id}", "invalido"))
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `Listar todos los duenios`() {
+        mockMvc
+            .perform(get("/duenios"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.length()").value(3))
+    }
+
+    @Test
+    @Transactional
+    fun `Crear un nuevo duenio con un payload valido`() {
+        val body = mapOf("nombreCompleto" to "Fernando Dodino")
+        val json = JSONObject(body).toString()
+
+        mockMvc
+            .perform(
+                post("/duenios")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+            )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.nombreCompleto").value("Fernando Dodino"))
+    }
+
+    @Test
+    fun `Intentar crear un duenio con payload invalido devuelve bad request`() {
+        mockMvc
+            .perform(
+                post("/duenios")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{}")
+            )
+            .andExpect(status().isBadRequest)
+    }
 }
