d182f05d22bd88167d12798e5d79c35d61ae1990
nicovio <nicolasviotti90@gmail.com>
2025-03-01 13:15:54 -0300

    Minor changes

--- src/main/kotlin/org/uqbar/heladeriakotlin/security/TokenUtils.kt
+++ src/main/kotlin/org/uqbar/heladeriakotlin/security/TokenUtils.kt
@@ -3,6 +3,7 @@ package org.uqbar.heladeriakotlin.security
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
@@ -21,7 +22,7 @@ class TokenUtils {
    @Value("\${security.access-token-minutes}")
    var accessTokenMinutes: Int = 30
 
-   val logger = LoggerFactory.getLogger(TokenUtils::class.java)
+   val logger: Logger = LoggerFactory.getLogger(TokenUtils::class.java)
 
    fun createToken(nombre: String, roles: List<String>): String? {
       val longExpirationTime = accessTokenMinutes.minutes.inWholeMilliseconds
--- src/main/kotlin/org/uqbar/heladeriakotlin/service/UsuarioService.kt
+++ src/main/kotlin/org/uqbar/heladeriakotlin/service/UsuarioService.kt
@@ -1,7 +1,6 @@
 package org.uqbar.heladeriakotlin.service
 
 import jakarta.transaction.Transactional
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -9,13 +8,12 @@ import org.springframework.stereotype.Service
 import org.uqbar.heladeriakotlin.dao.RepoUsuarios
 import org.uqbar.heladeriakotlin.dto.CredencialesDTO
 import org.uqbar.heladeriakotlin.errorHandling.CredencialesInvalidasException
+import org.uqbar.heladeriakotlin.model.Usuario
 import org.uqbar.heladeriakotlin.security.TokenUtils
 
 @Service
 @Transactional
 class UsuarioService : UserDetailsService {
-
-   val logger = LoggerFactory.getLogger(UsuarioService::class.java)
 
    @Autowired
    lateinit var usuarioRepository: RepoUsuarios
@@ -30,7 +28,7 @@ class UsuarioService : UserDetailsService {
       return tokenUtils.createToken(credencialesDTO.usuario, usuario.roles.map { it.name })!!
    }
 
-   fun validarUsuario(nombreUsuario: String) = usuarioRepository.findByUsername(nombreUsuario).orElseThrow { CredencialesInvalidasException() }
+   fun validarUsuario(nombreUsuario: String): Usuario = usuarioRepository.findByUsername(nombreUsuario).orElseThrow { CredencialesInvalidasException() }
 
    override fun loadUserByUsername(username: String?): UserDetails {
       if (username == null) throw CredencialesInvalidasException()
