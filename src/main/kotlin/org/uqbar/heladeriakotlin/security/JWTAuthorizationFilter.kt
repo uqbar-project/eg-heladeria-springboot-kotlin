6c47fb95a8b28d6305d83035dbec5615f3197fa3
Fernando Dodino <fernando.dodino@gmail.com>
2025-03-02 14:24:57 -0300

    Removing unused imports

--- src/main/kotlin/org/uqbar/heladeriakotlin/security/JWTAuthorizationFilter.kt
+++ src/main/kotlin/org/uqbar/heladeriakotlin/security/JWTAuthorizationFilter.kt
@@ -4,11 +4,9 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import org.uqbar.heladeriakotlin.errorHandling.CredencialesInvalidasException
 import org.uqbar.heladeriakotlin.service.UsuarioService
 
 @Component
