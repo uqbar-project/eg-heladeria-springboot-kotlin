79750faf88f9cc18f9a083f284d713d7fd927d1c
Fernando Dodino <fernando.dodino@gmail.com>
2025-03-03 20:19:55 -0300

    Add specific error

--- src/main/kotlin/org/uqbar/heladeriakotlin/security/JWTAuthorizationFilter.kt
+++ src/main/kotlin/org/uqbar/heladeriakotlin/security/JWTAuthorizationFilter.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
+import org.uqbar.heladeriakotlin.errorHandling.CredencialesInvalidasException
 import org.uqbar.heladeriakotlin.service.UsuarioService
 
 @Component
@@ -21,11 +22,16 @@ class JWTAuthorizationFilter : OncePerRequestFilter() {
    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
       val bearerToken = request.getHeader("Authorization")
       if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-         val token = bearerToken.substringAfter("Bearer ")
-         val usernamePAT = tokenUtils.getAuthentication(token)
-         usuarioService.validarUsuario(usernamePAT.name)
-         SecurityContextHolder.getContext().authentication = usernamePAT
-         logger.info("username PAT: $usernamePAT")
+         try {
+            val token = bearerToken.substringAfter("Bearer ")
+            val usernamePAT = tokenUtils.getAuthentication(token)
+            usuarioService.validarUsuario(usernamePAT.name)
+            SecurityContextHolder.getContext().authentication = usernamePAT
+            logger.info("username PAT: $usernamePAT")
+         } catch (e: CredencialesInvalidasException) {
+            response.status = 498 // Invalid Token (ESRI)
+            // Returned by ArcGIS for Server. Code 498 indicates an expired or otherwise invalid token
+         }
       }
       filterChain.doFilter(request, response)
    }
